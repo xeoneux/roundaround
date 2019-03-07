3c32e9e05931bb120f90be0174e5208c01b421f2
Aayush Kapoor <xeoneux@icloud.com>
2019-03-07 15:33:49 +0530

    Load mapbox gl lib with env api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,26 @@
 import React from "react";
 
-export default () => <h1>roundAround</h1>;
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default class Map extends React.Component {
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={process.env.MAPBOX}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
+    );
+  }
+}
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import next from "next";
 import express from "express";
+import webpack from "webpack";
 import { config } from "dotenv";
+import withCSS from "@zeit/next-css";
 
 import routes from "./routes";
 
-config();
-
-const app = express();
-const server = next({
-  dev: app.get("env")
+const { parsed } = config();
+const server = express();
+const app = next({
+  dev: server.get("env"),
+  conf: withCSS({
+    webpack(configuration) {
+      const env = new webpack.EnvironmentPlugin(parsed);
+      configuration.plugins.push(env);
+      return configuration;
+    }
+  })
 });
 
 (async () => {
-  await server.prepare();
-  app.use(routes.getRequestHandler(server));
-  app.listen(process.env.PORT);
+  await app.prepare();
+  server.use(routes.getRequestHandler(app));
+  server.listen(process.env.PORT);
 })();
